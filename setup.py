11d2a21650134098f3c8489c838ac9d0b32c427f
romanchemist <79202454+romanchemist@users.noreply.github.com>
2021-04-29 21:05:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='conformer',
+    name='PrirodaOptimiser',
     version='0.1.0',
     license='CIMM.07-013',
     author="Dmitry x2, Zhenya x2, Darya, Polina, Il'nura, Roma, Anya",
