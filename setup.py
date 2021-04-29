160e08f956443898a6077d8c10e7c7b1548566b9
zandmitrij <zandmitrij@gmail.com>
2021-04-29 22:00:32 +0300

    something was happened

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
 setup(
+<<<<<<< HEAD
     name='PrirodaOptimizer',
+=======
+    name='PrirodaOptimiser',
+>>>>>>> 11d2a21... Update setup.py
     version='0.1.0',
     license='CIMM.07-013',
     author="Dmitry x2, Zhenya x2, Darya, Polina, Il'nura, Roma, Anya",
