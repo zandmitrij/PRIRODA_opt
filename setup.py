280ae7b272030247a998fb8ae0676e40cc3a63b8
Dmitry-BV <69424429+Dmitry-BV@users.noreply.github.com>
2021-04-29 18:59:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='conformer',
     version='0.1.0',
+    license='CIMM.07-013',
+    author="Dmitry x2, Zhenya x2, Darya, Polina, Il'nura, Roma, Anya",
+    url='http://www.onlyfans.com',
     packages=['PrirodaOptimiser'],
     zip_safe=False,
 )
